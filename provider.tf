05ac6395b6fbb2bbde563a38abe16625b62a5361
Idris Fagbemi <idrisniyi94@gmail.com>
2023-10-03 21:28:07 -0500

    Creating eks cluster

--- provider.tf
+++ provider.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "devops-lab-2023"
     workspaces {
-    #   prefix = "devops-eks-demo-"
-    name = "devops-eks-demo"
+      #   prefix = "devops-eks-demo-"
+      name = "devops-eks-demo"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-west-2"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
--- variable.tf
+++ variable.tf
@@ -1,2 +1,45 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+
+# --------------------------
+# - VPC AND SUBNET
+#---------------------------
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "private_cidr_block" {
+  type = string
+}
+
+variable "public_cidr_block" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
+
+
+# ----------------------------------------
+# - EKS CLUSTER
+# ----------------------------------------
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+}
+
+variable "instance_type" {
+  type = list(string)
+}
+
+variable "max_capacity" {
+  type = number
+}
+
+variable "min_capacity" {
+  type = number
+}
